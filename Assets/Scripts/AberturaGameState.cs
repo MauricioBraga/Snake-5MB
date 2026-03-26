22347bd2dfb0508ff2d372e6424fb994c9c7f14f
Maurício Braga <mb@gmail.com>
2026-03-26 11:58:51 -0300

    Adicionado  o som da tela inicial.

--- Assets/Scripts/AberturaGameState.cs
+++ Assets/Scripts/AberturaGameState.cs
@@ -7,6 +7,7 @@ public class AberturaGameState: GameBaseState
     private float timer;
     private int contador;
 
+    private GameObject sfxTelaInicialJogo;
     public override void enterState(GameStateManager gameState)  {
         Debug.Log("entramos na tela inicial");
         telaInicialJogo = GameObject.Find("tela_nova_inicial_Snake_1280_1060_0");
@@ -14,6 +15,9 @@ public class AberturaGameState: GameBaseState
 
         timer = 0;
         contador = 20;
+        sfxTelaInicialJogo = GameObject.Find("snake-hissing");
+        sfxTelaInicialJogo.GetComponentInParent<AudioSource>().Play();
+
     }
     public override void updateState(GameStateManager gameState)  {
 
@@ -35,6 +39,8 @@ public class AberturaGameState: GameBaseState
     public override void leaveState(GameStateManager gameState)  {
         Debug.Log("saímos da tela inicial");
         telaInicialJogo.GetComponent<SpriteRenderer>().enabled = false;
+        sfxTelaInicialJogo.GetComponentInParent<AudioSource>().Stop();
+                                        
     }
 }
 
