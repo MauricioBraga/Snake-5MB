ce66f5707deb3d2307dcadb88aa6242d96f1a045
Maurício Braga <mb@gmail.com>
2026-03-26 10:45:57 -0300

    Criado o script de controle do personagem (cobra).

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour   {
+    public int score;
+    public Vector2Int direction = Vector2Int.right;
+    private Vector2Int input;
+    public List<Transform> segments = new List<Transform>();
+    public Transform segmentPreFab;
+    public bool ativo = false;
+    public GameStateManager gameState;
+
+    public void Start()        {
+         ativo = true; // remover essa linha depois.
+        
+        // Reseta a cobra para o tamanho inicial.
+        // Faz isso limpando a lista, adicionando novamente a “cabeça”.       
+        segments.Clear();
+        segments.Add(transform);
+        score = 0;
+        direction = Vector2Int.right;
+    }
+
+    public void setAtivo(bool estado_player)    {
+       ativo = estado_player;
+    }
+
+ private void Update()    {
+        if (!ativo)
+            return;
+        
+        // Only allow turning up or down while moving in the x-axis
+        if (direction.x != 0f)      {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))    {
+                direction = Vector2Int.up;
+            }
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))     {
+                direction = Vector2Int.down;
+            }
+        }
+        
+        // Só pode mover para a esquerda ou direita se estiver movendo na        
+        // direção Y.
+        else if (direction.y != 0f)    {
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                direction = Vector2Int.right;
+            }
+            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                direction = Vector2Int.left;
+            }
+        }
+    }
+    
+
+ private void FixedUpdate()    {
+        if (!ativo)
+            return;
+        // Set each segment's position to be the same as the one it follows. We
+        // must do this in reverse order so the position is set to the previous
+        // position, otherwise they will all be stacked on top of each other.
+        for (int i = segments.Count - 1; i > 0; i--)     {
+              segments[i].position = segments[i - 1].position;
+        }
+        // Move the snake in the direction it is facing
+        // Round the values to ensure it aligns to the grid
+        int x = Mathf.RoundToInt(transform.position.x) + direction.x;
+        int y = Mathf.RoundToInt(transform.position.y) + direction.y;
+        transform.position = new Vector2(x, y);
+    }
+
+}
