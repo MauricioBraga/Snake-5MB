0380c507b9182cbbd672c626bb1be2f6c9508ad0
Maurício Braga <mb@gmail.com>
2026-04-30 11:15:47 -0300

    Adicionada a música de gameplay

--- Assets/Scripts/JogoGameState.cs
+++ Assets/Scripts/JogoGameState.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class JogoGameState: GameBaseState
 {
+    private GameObject gameMusic;
     public override void enterState(GameStateManager gameState)  {
         Debug.Log("entramos no modo jogo.");
         gameState.AtivarElementosJogo(true);
         gameState.player.GetComponent<Snake>().ResetState();
+        gameMusic = GameObject.Find("Tight_Turns");
+        gameMusic.GetComponentInParent<AudioSource>().Play();
+
     }
     public override void updateState(GameStateManager gameState)  {
        if (Input.GetKeyDown(KeyCode.Space))  {
@@ -16,6 +20,8 @@ public class JogoGameState: GameBaseState
     public override void leaveState(GameStateManager gameState)  {
         Debug.Log("saímos do modo jogo");
         gameState.AtivarElementosJogo(false);
+        gameMusic.GetComponentInParent<AudioSource>().Stop();
+
     }
 }
 
